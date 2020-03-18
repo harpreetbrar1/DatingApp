75e4d6eb881fabe6162841432860fbcbfdfbd104
harpreetbrar1 <leopardbrar@gmail.com>
2020-03-18 08:06:28 -0700

    Client Side Login and Register Completed

---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,32 @@
+import { AuthService } from './../_services/auth.service';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+// tslint:disable-next-line: no-unused-expression
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {}
+  register() {
+    this.authService.register(this.model).subscribe(
+      () => {
+        console.log('Registration Successful');
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+  cancel() {
+    this.cancelRegister.emit(false);
+    console.log('cancelled');
+  }
+}
